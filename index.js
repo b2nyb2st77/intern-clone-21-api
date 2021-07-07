d1fefb8289a7327c047e3a5b47e348031eafe2b7
soobeen <b2nyb2st77@gmail.com>
2021-07-07 11:01:18 +0900

    location api

--- index.js
+++ index.js
@@ -2,52 +2,33 @@ const { response } = require("express");
 const express = require("express");
 const app = express();
 const port = 3000;
-const mysql = require("mysql");
+const location = require("./location");
+const car = require("./car");
 
-const con = mysql.createConnection({
-    host: 'teamo2-test.c0dbvvfuggmr.ap-northeast-2.rds.amazonaws.com',
-    user: 'teamo2_intern',
-    password: 'teamo0924',
-    database: 'carmore'
-});
-
-con.connect(function(err){
-    if(err) throw err;
-    console.log("mysql is connected");
-
-    // con.query('CREATE DATABASE carmore', function(err, result){
-    //     if(err) throw err;
-    //     console.log('database created');
-    // })
-})
-
+// 시작
 app.get("/", function(req, res){
     res.send("Hello World!\n");
 });
 
-app.get("/cars", function(req, res){
-    const sql = "select * from car";
-    con.query(sql, function(err, result, fields){
+app.use("/location", location);
+app.use("/car", car);
+
+// index로 렌트카 업체 하나의 정보 불러오기
+app.get("/affiliate/:index", function(req, res){
+    const sql = "select * from affiliate where a_index = ?";
+    con.query(sql, [req.params.index], function(err, result, fields){
         if(err) throw err;
-        console.log(result);
         res.send(result);
     });
-})
+});
 
-app.get("/locations", function(req, res){
-    const sql = "select * from location";
-    con.query(sql, function(err, result, fields){
-        if(err) throw err;
-        console.log(result);
-        res.send(result);
-    });
-})
-
+// 없는 api일 때
 app.use(function(res, res, next){
     res.status(404).send("NOT FOUND\n");
     next();
 });
 
+// 서버 연결 완료
 app.listen(port, function(){
     console.log("simple api server is open");
 });
