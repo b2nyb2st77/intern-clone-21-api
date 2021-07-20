c67244b94d2c75ad1e5cbcc4b9f4b38470f2ddf6
soobeen <b2nyb2st77@gmail.com>
2021-07-20 12:19:28 +0900

    피드백 반영9
    템플릿 리터럴 적용하기

--- db/location.js
+++ db/location.js
@@ -14,7 +14,7 @@ module.exports = {
         else if (type === 'airport' || type === 'ktx' || type === 'srt' || type === 'bus' || type === 'region' || type === 'abroad') {
             sql = `SELECT DISTINCT l_name, l_immediate_or_not 
                    FROM location
-                   WHERE l_type = '` + type + `'`;         
+                   WHERE l_type = ${type}`;         
         }
 
         return connection.query(sql, function(err, result){
@@ -26,7 +26,7 @@ module.exports = {
     searchLocation: (searchWord, callback) => {
         const sql = `SELECT DISTINCT l_name, l_immediate_or_not 
                      FROM location
-                     WHERE l_name LIKE '%` + searchWord + `%'`;         
+                     WHERE l_name LIKE '%${searchWord}%'`;         
 
         return connection.query(sql, function(err, result){
             if(err) throw err;
